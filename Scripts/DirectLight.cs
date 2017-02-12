05e4bdc166e87b71543e7a6159fa8caf23f78340
ZzzzzGuo <zg2808@g.rit.edu>
2017-02-12 13:43:53 -0500

    Direct Light Intensity Control System
    Press "W" intensity up, press "S" intensity down.
    
    Reference: Linear Interpolation 
    https://unity3d.com/learn/tutorials/topics/scripting/linear-interpolation?playlist=17117

---
+++ Scripts/DirectLight.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectLight : MonoBehaviour
+{
+
+    public Light light;
+    // Use this for initialization
+    void Start()
+    {
+        light = GetComponent<Light>();
+        light.intensity = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Manully adjust light intensity.
+        if (Input.GetKey(KeyCode.W))
+        {
+            light.intensity = Mathf.Lerp(light.intensity, 8f, 0.002f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            light.intensity = Mathf.Lerp(light.intensity, 0, 0.02f);
+        }
+        //Auto ajust light intensity
+        //light.intensity = Mathf.Lerp(light.intensity, 0f, 0.002f * Time.deltaTime);
+    }
+}
