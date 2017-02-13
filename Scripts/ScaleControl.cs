3317e3db740ee8a75ccebea2c0339265e80b0e75
ZzzzzGuo <zg2808@g.rit.edu>
2017-02-13 00:08:28 -0500

    Add files via upload
    Using Function GetAxis to control gameObject's scale.

---
+++ Scripts/ScaleControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleControl : MonoBehaviour
+{
+
+    float speedScale = 5.0f;
+    float xScale;
+    float zScale;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xScale = Input.GetAxis("Horizontal") * Time.deltaTime * speedScale;
+        zScale = Input.GetAxis("Vertical") * Time.deltaTime * speedScale;
+        transform.localScale += new Vector3(xScale, 0, zScale);
+    }
+}
